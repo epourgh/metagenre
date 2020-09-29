12e80c03d77d70f6e0820f898bbd701f31397ec2
Emerson Pourghaed <ampourgh@gmail.com>
2020-09-29 15:47:42 -0500

    misc

--- metagenre-frontend/src/components/relationships/subgenres.component.js
+++ metagenre-frontend/src/components/relationships/subgenres.component.js
@@ -4,6 +4,7 @@ import { GlobalContext } from '../../context/GlobalState';
 function getWindowParam() {
     var url_string = window.location.href;
     var url = new URL(url_string);
+    console.log(`id: ${url.searchParams.get("id")}`)
     const id = url.searchParams.get("id").toString();
     const title = capitalizeFirstLetter(url.searchParams.get("title").toString());
     const windowsParams = [id, title];
@@ -18,17 +19,25 @@ function capitalizeFirstLetter(string) {
 export default function RelationshipsSubgenres() {
     
     const {loggedIn, setLoggedIn} = useContext(GlobalContext)
-    const [id, title] = getWindowParam();
+    const [genre, setGenre] =  useState({
+        id: 0,
+        title: 'none'
+    });
     const [subgenres, setSubgenres] = useState([]);
     const [subgenresFiltered, setSubgenresFiltered] = useState([]);
     const [searchValue, setSearchValue] = useState('');
     
 
     useEffect(() => {
-        getGenres();
+        const [id, title] = getWindowParam();
+        setGenre({
+            id: id,
+            title: title
+        })
+        getSubgenres();
     }, [])
 
-    const getGenres = (stringParam) => {
+    const getSubgenres = (stringParam) => {
         fetch(`http://localhost:4000/subgenres`)
             .then(response => response.json())
             .then(response => {
@@ -64,7 +73,7 @@ export default function RelationshipsSubgenres() {
             .then(response => response.json())
             .then(response => {
                  if (response.data.length > 0) {
-                     console.log(response.data[0].genresId)
+                     console.log(response.data[0])
                  } else {
                      console.log('doesn\'t exist');
                  }
@@ -75,7 +84,7 @@ export default function RelationshipsSubgenres() {
 
     return (
         <div className="bodyContentStyling">
-            <h2><b>Genre:</b> {title}</h2>
+            <h2><b>Genre:</b> {genre.title}</h2>
             <h3>Vote on a Subgenre:</h3>
             <form onKeyUp={e => searchHandler()} onSubmit={searchHandler}>
                 <input
@@ -92,7 +101,7 @@ export default function RelationshipsSubgenres() {
                 subgenresFiltered.map(subgenre => {
                     return (
                         <li>
-                            <a href="#" onClick={() => voteSubgenreIntoGenre(id, subgenre.id)}><b>{subgenre.name}</b></a>{" "}
+                            <a href="#" onClick={() => voteSubgenreIntoGenre(genre.id, subgenre.id)}><b>{subgenre.name}</b></a>{" "}
                         </li>
                     );
                 })
