573e538cd5e430ee1ea1a878fd8ac47150d5bda1
Emerson Pourghaed <ampourgh@gmail.com>
2020-11-30 11:48:19 -0600

    middleware multiple fetch requests loop

--- metagenre-frontend/src/context/middleware/async.js
+++ metagenre-frontend/src/context/middleware/async.js
@@ -1,18 +1,34 @@
 export default function dispatchMiddleware(dispatch) {
   return (action) => {
 
-    if (action.type.match(/fetch-/i)) {
+        if (action.type.match(/fetch-/i)) {
 
-        fetch(action.payload.url)
-            .then(response => response.json())
-            .then(response => {
-                dispatch({type: action.type, payload: { section: action.payload.section, data: response.data } })
-            })
+            if (action.type.match(/fetch-multiple/i)) {
             
-    } 
+                const resultsObj = action.payload.url.map((url) => {
+                    fetch(url)
+                        .then(response => response.json())
+                        .then(response => {
+                            return response.data;
+                        })
+                });
 
-    console.log(`pass throuh async: ${action.type}`)
-    return dispatch(action);
+                return dispatch({type: action.type, payload: resultsObj })
+                
+            }
+
+            fetch(action.payload.url)
+                .then(response => response.json())
+                .then(response => {
+                    return dispatch({type: action.type, payload: response.data })
+                })
+
+        }
+
+        console.log(`pass throuh async: ${action.type}`)
+        return dispatch(action);
+        
+    }
+
     
-  };
-}
+};
